d6a90b68eea8927ec31dfc5fe156c07b0fbfe6c3
lakshmiChakra <mailtolakshmi.ks@gmail.com>
2022-01-14 23:00:42 +0530

    Initial commit

---
+++ angularfront/src/app/employee.ts
@@ -0,0 +1,16 @@
+export class Employee {
+   constructor(
+       public id:number,
+       public firstName:string,
+       public secondName:string,
+       public emailId:string
+
+       ){}
+   
+    }
+  
+    
+    
+    
+
+
